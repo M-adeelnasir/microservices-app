8a14facdf4aae26cedee471299696242663436d2
unknown <adeelnasirkbw@gmail.com>
2022-09-07 06:41:16 -0700

    nginx ingress controller and ingress-srv file

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.get('/api/health-check', (req, res) => {
+  res.send('Health Check');
+});
+
 app.listen(PORT, () => {
-  console.log('Server is listening at port 4000');
+  console.log('Server is listening at port 4000!!');
 });
