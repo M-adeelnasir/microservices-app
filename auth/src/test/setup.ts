b48085df5e696d1597e4535e14b33c525985eb97
unknown <adeelnasirkbw@gmail.com>
2022-09-12 14:01:06 -0700

    jest and mongodb-memory-server setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../services/app';
+
+let mongo: any;
+//@ts-ignore
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri);
+});
+
+//@ts-ignore
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+//@ts-ignore
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
