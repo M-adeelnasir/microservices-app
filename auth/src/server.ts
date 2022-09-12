13996cab545e5d2a3c51d67a4fac7a47f5366bd3
unknown <adeelnasirkbw@gmail.com>
2022-09-12 13:27:48 -0700

    server.ts code refactore

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,45 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import cookieSeesion from 'cookie-session';
-import { currentUserRouter } from './routes/currentUser';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middleware/errorResponse';
-import { NotFoundError } from './errors/notFound-error';
 import connectDB from './services/db';
+import { app } from './services/app';
 
-const app = express();
-app.use(express.json());
 const PORT = process.env.PORT || 4000;
-
-app.set('trust proxy', true);
-
-app.use(
-  cookieSeesion({
-    signed: false, //encrypt fales
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.get('/api/health-check', (req, res) => {
-  res.sendStatus(200);
-});
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
-
-//without express-async-errors
-// app.all('*', async (req, res, next) => {
-//   next(new NotFoundError());
-// });
 
 app.listen(PORT, async () => {
   console.log('Server is listening at port 4000!!');
---
+++ auth/src/services/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSeesion from 'cookie-session';
+import { currentUserRouter } from '../routes/currentUser';
+import { signinRouter } from '../routes/signin';
+import { signupRouter } from '../routes/signup';
+import { signoutRouter } from '../routes/signout';
+import { errorHandler } from './../middleware/errorResponse';
+import { NotFoundError } from './../errors/notFound-error';
+
+const app = express();
+app.use(express.json());
+
+app.set('trust proxy', true);
+
+app.use(
+  cookieSeesion({
+    signed: false, //encrypt fales
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.get('/api/health-check', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+//without express-async-errors
+// app.all('*', async (req, res, next) => {
+//   next(new NotFoundError());
+// });
+
+export { app };
