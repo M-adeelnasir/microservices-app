fabc83c91eb17354629040e453839f930b691dc7
unknown <adeelnasirkbw@gmail.com>
2022-09-14 13:08:46 -0700

    sign in page complete with req

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import useRequest from '../../hooks/use-request'
+import Router from 'next/router'
+
+const signin = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const { doRequest, errors } = useRequest({ method: 'post', url: 'https://auth.dev/api/users/signin', body: { email, password }, onSuccess: () => Router.push('/') })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await doRequest()
+    }
+
+    return (
+        <>
+            <div className="container w-100">
+                <h1>signin</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Your Email</label>
+                        <input type="text" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Your Password</label>
+                        <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    {errors}
+                    <button className="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+
+export default signin
