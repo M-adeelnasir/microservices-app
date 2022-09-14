96c56365eb9aeb06d8f60678939c79f9cade7af4
unknown <adeelnasirkbw@gmail.com>
2022-09-14 13:21:15 -0700

    getinitalprops in-app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,32 @@
 import Head from "next/head";
+import BuildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
     return (<>
 
         <Head>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
         </Head>
 
-        <Component {...pageProps} />
+        <>
+
+            <Component {...pageProps} />
+        </>
     </>)
 }
+
+
+
+AppComponent.getInitialProps = async (context) => {
+    // console.log(context.ctx.req);
+    const client = await BuildClient(context.ctx)
+    const { data } = await client.get('/api/users/currentuser')
+    // console.log(data);
+    let pageProps;
+    if (context.Component.getInitialProps) {
+        pageProps = await context.Component.getInitialProps(context.ctx)
+    }
+    return { data, pageProps }
+}
+
+export default AppComponent
