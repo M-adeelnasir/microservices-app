c68b7e31ca301c8c28b027eafb57cb4716699311
unknown <adeelnasirkbw@gmail.com>
2022-09-19 12:48:55 -0700

    nats connection close

--- tickets/src/services/natsConnect.ts
+++ tickets/src/services/natsConnect.ts
@@ -3,6 +3,14 @@ import { natsWrapper } from './stanWrapper';
 export const natsConnect = async () => {
   try {
     await natsWrapper.connect('auth', 'sjhdjhs', 'http://nats-srv:4222');
+
+    natsWrapper.client.on('close', () => {
+      console.log('Nats connection closed');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
   } catch (err) {
     console.log(err);
   }
