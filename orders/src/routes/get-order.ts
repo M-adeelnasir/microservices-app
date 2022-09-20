de5dbe990a46790fc6fb315a7b1c9faba4ae53a8
unknown <adeelnasirkbw@gmail.com>
2022-09-20 14:45:37 -0700

    get orders

--- orders/src/routes/get-order.ts
+++ orders/src/routes/get-order.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
 
-router.get('api/orders', async (req: Request, res: Response) => {
+router.get('api/orders/:id', async (req: Request, res: Response) => {
   res.send({});
 });
 
--- orders/src/routes/get-orders.ts
+++ orders/src/routes/get-orders.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express';
+import { Order } from '../model/orders';
+
 const router = express.Router();
 
-router.get('api/orders/:id', async (req: Request, res: Response) => {
-  res.send({});
+router.get('api/orders', async (req: Request, res: Response) => {
+  const orders = await Order.find({ userId: req.user!.id }).populate(
+    'ticketId'
+  );
+
+  res.send({ orders });
 });
 
 export { router as ordersGetRoute };
